95de1480e93aa91f407fd58333d38a3a1316b5b7
Ajaymenonm <ammuralidhar@qwinixtech.com>
2016-07-13 10:10:15 +0530

    add player basic intent and age intent

--- NFL/index.js
+++ NFL/index.js
@@ -12,14 +12,15 @@ var options = {
 
 var _ = require('lodash');
 var Alexa = require('alexa-app');
-var app = new Alexa.app('nfl'); // Define an alexa-app [use the same name as invocation name]
+var app = new Alexa.app('nfl players'); // Define an alexa-app [use the same name as invocation name]
 var fantasyData = require('fantasydata-api')(options);
 var playerId = require('./player_ids');
 
 // Gets called whenever there is a launch request
 app.launch(function(req, res) {
-  var prompt = 'Welcome to NFL skill.';
-  res.say(prompt).reprompt(prompt).shouldEndSession(false);
+  var prompt = 'Welcome to NFL skill.',
+  reprompt = 'Are you still there? You can ask me details of a player like, his age, height, his current team and so forth.';
+  res.say(prompt).reprompt(reprompt).shouldEndSession(false);
 });
 
 // Player Basic Introduction Intent
@@ -36,25 +37,24 @@ function(req, res){
   var prompt = '';
   
   if (information) {
-    fantasyData.nfl.player(information, function(err, results) {
+    fantasyData.nfl.player(information, function(err, results) {  
       prompt = _.template('${name} was born on ${dob}. He currently plays for ${team} at ${position} position.')({
         name: results.Name,
         dob: results.BirthDateString,
         team: results.CurrentTeam,
         position: results.PositionCategory
       });
-      // console.log(prompt)
+      res.say(prompt).reprompt(reprompt).card({
+        type: "Standard",
+        title: "Player Basic Introduction",
+        text: information,
+        image: {                //image is optional 
+          smallImageUrl: "https://s3.amazonaws.com/qwinix-echo/small.jpg",
+          largeImageUrl: "https://s3.amazonaws.com/qwinix-echo/large.jpg"
+        } 
+      }).shouldEndSession(false).send();
     }); 
-    res.say(prompt).reprompt(reprompt).card({
-      type: "Standard",
-      title: "Player Basic Introduction",
-      text: information,
-      image: {                //image is optional 
-        smallImageUrl: "https://s3.amazonaws.com/qwinix-echo/small.jpg",
-        largeImageUrl: "https://s3.amazonaws.com/qwinix-echo/large.jpg"
-      } 
-    }).shouldEndSession(false).send();
-    return true;
+    return false;
   } else {
     var speech;
     var reprompt = 'What else can I help you with?' 
@@ -66,5 +66,60 @@ function(req, res){
     res.say(speech).reprompt(reprompt).shouldEndSession(false);
   }
 });
+
+// Player Age Intent
+app.intent('playerAge', {
+  'slots': {
+    'Player': 'LIST_OF_PLAYERS',
+    'Age': 'AGE'
+  }
+}, 
+function(req, res){
+  // get the slot
+  var infoSlot = req.slot('Player').toLowerCase();
+  var information = playerId[infoSlot],
+  reprompt = 'Tell me a player name';
+  var prompt = '';
+  
+  if (information) {
+    fantasyData.nfl.player(information, function(err, results) {  
+      prompt = _.template('${name} is ${age} years old')({
+        name: results.Name,
+        age: results.Age
+      });
+      res.say(prompt).reprompt(reprompt).card({
+        type: "Standard",
+        title: "Player Age",
+        text: information,
+        image: {                //image is optional 
+          smallImageUrl: "https://s3.amazonaws.com/qwinix-echo/small.jpg",
+          largeImageUrl: "https://s3.amazonaws.com/qwinix-echo/large.jpg"
+        } 
+      }).shouldEndSession(false).send();
+    }); 
+    return false;
+  } else {
+    var speech;
+    var reprompt = 'What else can I help you with?' 
+    if (infoSlot) {
+      speech = "I'm sorry, I currently do not have any Information for " + infoSlot + ". What else can I help you with?";
+    } else {
+      speech = "I'm sorry, I currently do not have any Information regarding that. What else can I help you with?";
+    }
+    res.say(speech).reprompt(reprompt).shouldEndSession(false);
+  }
+});
+
+app.intent('AMAZON.HelpIntent',
+  function (req, res) {
+    var help = "You can ask me details of a player like, his age, height, his current team and so forth, or, you can say exit... Now, what can I help you with? ";
+    res.say(help).shouldEndSession(false);              // Or let the user stop an action (but remain in the skill)
+  });
+
+app.intent('AMAZON.StopIntent',
+  function (req, res) {
+    var goodbye = "Had a nice time talking to you. Goodbye.";
+    res.say(goodbye).shouldEndSession(true);              // Or let the user stop an action (but remain in the skill)
+  });
 
 module.exports = app;
